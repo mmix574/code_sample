874b0ec7869b9d11d9ffa06df1cfbf8fb82ddfed
taita2015 <mmix.xia@gmail.com>
2017-03-06 19:01:19 +0800

    0306

---
+++ sort/merge_sort_fix.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+void merge_sort(int A[],int n){
+	
+}
+void msort(int A[],int start,int end){
+
+}
+void merge(int A[],int B[],int n){
+	
+}
+
+#define ELEMENT_SIZE 10
+#define MAX_ARRAY_SIZE 100
+
+int main(){
+	int A[MAX_ARRAY_SIZE];
+	
+	return 0;
+}
