108c400c35a7a737b75a19c5efadaae35127a7d0
taita2015 <mmix.xia@gmail.com>
2017-03-09 17:39:22 +0800

    0309

---
+++ binary decimal/decimal2binary.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int len (char *origin){
+	int len = 0;
+	while(origin[len]!='\0'){
+		len++;
+	}
+	return len;
+}
+
+char *decimal2binary(char *origin){
+	int size = len(origin);
+	char rest[32] ;
+	 
+}
+
+int main(){
+	char buf[100];
+	scanf("%s",buf);
+
+	return 0;
+}
