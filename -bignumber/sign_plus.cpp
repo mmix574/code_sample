6b9270290c54ca279be5e0c1ae6c61d3dc000fda
taita2015 <mmix.xia@gmail.com>
2017-03-11 17:50:21 +0800

    0311

---
+++ -bignumber/sign_plus.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int len(char *p){
+	if(!p){
+		return -1;
+	}
+	int len = 0;
+	while(p[len]!='\0'){
+		len++;
+	}
+	return len;
+}
+
+bool sign(char *A){
+
+}
+char *abs(char *A){
+
+}
+char *sign_plus(char* A,char* B){
+
+}
+
+int main(){
+	return 0;
+}
